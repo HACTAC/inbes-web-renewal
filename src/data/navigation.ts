f2faf3c2d8f79f3b2ba9d868d33cede73b6a84ba
Takiwaki <66287298+takiwakidaisuke@users.noreply.github.com>
2026-07-02 05:38:51 +0900

    feat: simplify top entrance

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,6 +1,6 @@
 export const navigation = [
   { label: "自社製品", href: "/products/" },
-  { label: "開発支援", href: "/services/" },
+  { label: "商品化支援", href: "/services/" },
   { label: "開発実績", href: "/works/" },
   { label: "会社概要", href: "/company/" },
   { label: "お問い合わせ", href: "/contact/", emphasis: true }
